0894888b276cbdfdea8beda5b024222765777079
Tai Sakuma <tai.sakuma@gmail.com>
2020-10-30 13:07:50 -0400

    add initial code

--- nextlinegraphql/__init__.py
+++ nextlinegraphql/__init__.py
@@ -1,3 +1,55 @@
+'''
+uvicorn nextlinegraphql:app
+'''
+
+import asyncio
+from ariadne import (
+    gql,
+    QueryType,
+    SubscriptionType,
+    make_executable_schema
+    )
+from ariadne.asgi import GraphQL
+
+##__________________________________________________________________||
+type_def = gql("""
+    type Query {
+        hello: String!
+    }
+
+    type Subscription {
+        counter: Int!
+    }
+""")
+
+##__________________________________________________________________||
+query = QueryType()
+subscription = SubscriptionType()
+
+@query.field("hello")
+async def resolve_hello(_, info):
+    request = info.context["request"]
+    user_agent = request.headers.get("user-agent", "guest")
+    return "Hello, %s!" % user_agent
+
+
+@subscription.source("counter")
+async def counter_generator(obj, info):
+    for i in range(5):
+        await asyncio.sleep(1)
+        yield i
+
+
+@subscription.field("counter")
+def counter_resolver(count, info):
+    return count + 1
+
+
+##__________________________________________________________________||
+schema = make_executable_schema(type_def, [query, subscription])
+app = GraphQL(schema, debug=True)
+
+##__________________________________________________________________||
 
 ##__________________________________________________________________||
 from ._version import get_versions
