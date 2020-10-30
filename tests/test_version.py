3bea9309aef47dbdcbf2cbe344f68fd0d87bd02b
Tai Sakuma <tai.sakuma@gmail.com>
2020-10-30 12:13:31 -0400

    add tests

---
+++ tests/test_version.py
@@ -0,0 +1,9 @@
+import nextline
+
+##__________________________________________________________________||
+def test_version():
+    """test if the version string is attached to the module
+    """
+    nextline.__version__
+
+##__________________________________________________________________||
