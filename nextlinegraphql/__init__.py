9a61846aa8f03d19ae97440032acd47dbdafe45a
Tai Sakuma <tai.sakuma@gmail.com>
2020-10-30 12:55:18 -0400

    add comments

--- nextlinegraphql/__init__.py
+++ nextlinegraphql/__init__.py
@@ -1,4 +1,16 @@
 
+##__________________________________________________________________||
 from ._version import get_versions
 __version__ = get_versions()['version']
+"""str: version
+
+The version string, e.g., "0.1.2", "0.1.2+83.ga093a20.dirty".
+generated from git tags by versioneer.
+
+Versioneer: https://github.com/warner/python-versioneer
+
+"""
+
 del get_versions
+
+##__________________________________________________________________||
