2ef6aa880e90c092b76a29c5b3946c41d223e89a
Tai Sakuma <tai.sakuma@gmail.com>
2020-11-04 11:57:27 -0500

    add Mutation exec

--- nextlinegraphql/bindables/__init__.py
+++ nextlinegraphql/bindables/__init__.py
@@ -1,7 +1,8 @@
+from pathlib import Path
 import asyncio
-from ariadne import QueryType, SubscriptionType
 from ariadne import QueryType, MutationType, SubscriptionType
 
+from nextline import Nextline
 
 ##__________________________________________________________________||
 query = QueryType()
@@ -25,5 +26,29 @@ async def counter_generator(obj, info):
 @subscription.field("counter")
 def counter_resolver(count, info):
     return count + 1
+
+##__________________________________________________________________||
+_THIS_DIR = Path(__file__).resolve().parent
+statement = """
+import sys
+sys.path.insert(0, "{}")
+import script
+script.run()
+""".lstrip().format(_THIS_DIR)
+
+del _THIS_DIR
+
+breaks = {
+    '__main__': ['<module>'],
+    'script': ['run', 'task', 'task_imp', 'atask']
+}
+
+@mutation.field("exec")
+def resolve_exec(_, info):
+    print(_)
+    print(info)
+    nextline = Nextline(statement, breaks)
+    nextline.run()
+    return True
 
 ##__________________________________________________________________||
---
+++ nextlinegraphql/bindables/script.py
@@ -0,0 +1,41 @@
+import threading
+import asyncio
+
+##__________________________________________________________________||
+def task_imp():
+    tid = threading.get_ident()
+    for i in range(2):
+        print('in task() i={}'.format(i))
+
+def task():
+    return task_imp()
+
+def run_threads():
+    nthreads = 2
+    threads = [ ]
+    for i in range(nthreads):
+        t = threading.Thread(target=task)
+        t.start()
+        threads.append(t)
+        # time.sleep(0.1)
+    for t in threads:
+        t.join()
+
+async def atask():
+    print('here')
+
+async def run_coroutines():
+    a1 = atask()
+    a2 = atask()
+    a3 = atask()
+    await asyncio.gather(a1, a2, a3)
+
+def run():
+    run_threads()
+    asyncio.run(run_coroutines())
+
+##__________________________________________________________________||
+if __name__ == '__main__':
+    run()
+
+##__________________________________________________________________||
