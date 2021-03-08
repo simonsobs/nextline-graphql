a12e533d64e878a43633d8d6720d6a9bd73a572e
Tai Sakuma <tai.sakuma@gmail.com>
2021-03-08 16:50:19 -0500

    remove args

--- nextlinegraphql/__init__.py
+++ nextlinegraphql/__init__.py
@@ -12,6 +12,9 @@ from .schema import schema
 # app = CORSMiddleware(GraphQL(schema, debug=True), allow_origins=['*'], allow_methods=("GET", "POST", "OPTIONS"))
 app = CORSMiddleware(GraphQL(schema, debug=True), allow_origins=['*'], allow_methods=['*'], allow_headers=['*'])
 
+import sys
+sys.argv[1:] = []
+
 ##__________________________________________________________________||
 from ._version import get_versions
 __version__ = get_versions()['version']
