8a556b1e3cfaeab2a2dafd2b832381e7efe75083
Tai Sakuma <tai.sakuma@gmail.com>
2021-05-03 17:45:30 -0400

    update test_run.py

--- tests/schema/test_run.py
+++ tests/schema/test_run.py
@@ -1,4 +1,5 @@
 import sys
+import asyncio
 from async_asgi_testclient import TestClient
 
 import pytest
@@ -23,42 +24,6 @@ mutation SendPdbCommand(
 }
 '''.strip()
 
-SUBSCRIBE_STATE = '''
-subscription State {
-  state {
-    globalState
-    nthreads
-    threads {
-      threadId
-      tasks {
-        taskId
-        prompting
-        fileName
-        lineNo
-      }
-    }
-  }
-}
-'''.strip()
-
-QUERY_STATE = '''
-{
-  state {
-    globalState
-    nthreads
-    threads {
-      threadId
-      tasks {
-        taskId
-        prompting
-        fileName
-        lineNo
-      }
-    }
-  }
-}
-'''.strip()
-
 QUERY_GLOBAL_STATE = '''
 query GlobalState {
   globalState
@@ -80,53 +45,58 @@ subscription ThreadTaskIds {
 }
 '''.strip()
 
+SUBSCRIBE_THREAD_TASK_STATE = '''
+subscription ThreadTaskState(
+  $threadId: String!
+  $taskId: String
+) {
+  threadTaskState(threadId: $threadId, taskId: $taskId) {
+    prompting
+    fileName
+    lineNo
+  }
+}
+'''.strip()
+
 ##__________________________________________________________________||
-async def prompting_thread_tasks(ws):
-    prompting_counts_prev = {} # key (thread_id, task_id)
-    while True:
-        resp_json = await ws.receive_json()
-        state = resp_json['payload']['data']['state']
-        if not state['globalState'] == 'running':
-            return
-        threads = state['threads']
-        prompting_counts = {(th['threadId'], ta['taskId']): ta['prompting'] for th in threads for ta in th['tasks']}
-        thread_tasks = []
-        for thread_id_task_id in prompting_counts:
-            if thread_id_task_id in prompting_counts_prev:
-                if prompting_counts[thread_id_task_id] > prompting_counts_prev[thread_id_task_id]:
-                    thread_tasks.append(thread_id_task_id)
-            else:
-                thread_tasks.append(thread_id_task_id)
-        if thread_tasks:
-            yield thread_tasks
-        prompting_counts_prev = prompting_counts.copy()
+async def control_thread_task(client, thread_task_id):
+    print(f'control_thread_task({thread_task_id})')
 
-@pytest.mark.asyncio
-async def test_run(snapshot):
-
-    query_global_state = { 'query': QUERY_GLOBAL_STATE }
-
-    subscribe_global_state = {
+    subscribe_thread_task_state = {
         'id': '1',
         'type': 'start',
         'payload': {
-            'variables': {},
+            'variables': thread_task_id,
             'extensions': {},
             'operationName': None,
-            'query': SUBSCRIBE_GLOBAL_STATE
+            'query': SUBSCRIBE_THREAD_TASK_STATE
         }
     }
 
-    subscribe_state = {
-        'id': '1',
-        'type': 'start',
-        'payload': {
-            'variables': {},
-            'extensions': {},
-            'operationName': None,
-            'query': SUBSCRIBE_STATE
-        }
+    mutate_send_pdb_command = {
+        'query': MUTATE_SEND_PDB_COMMAND
     }
+
+    headers = {
+        'Content-Type:': "application/json"
+    }
+
+    async with client.websocket_connect("/") as ws:
+        await ws.send_json(subscribe_thread_task_state)
+        while True:
+            resp_json = await ws.receive_json()
+            if resp_json['type'] == 'complete':
+                break
+            state = resp_json['payload']['data']['threadTaskState']
+
+            if state['prompting']:
+                mutate_send_pdb_command['variables'] = {
+                    **thread_task_id,
+                    'command': 'continue'
+                }
+                resp = await client.post("/", json=mutate_send_pdb_command, headers=headers)
+
+async def control_execution(client):
 
     subscribe_thread_task_ids = {
         'id': '1',
@@ -139,71 +109,70 @@ async def test_run(snapshot):
         }
     }
 
-    query_state = { 'query': QUERY_STATE }
+    async with client.websocket_connect("/") as ws:
+        await ws.send_json(subscribe_thread_task_ids)
+        controllers = {}
+        while True:
+            resp_json = await ws.receive_json()
+            if resp_json['type'] == 'complete':
+                break
+            ids = resp_json['payload']['data']['threadTaskIds']
+            ids = [tuple(id_.items()) for id_ in ids] # because a dict cannot be a key of a dict
+            prev_ids = list(controllers.keys())
+            new_ids = [id_ for id_ in ids if id_ not in prev_ids]
+            ended_ids = [id_ for id_ in prev_ids if id_ not in ids]
+            for id_ in new_ids:
+                task = asyncio.create_task(control_thread_task(client, dict(id_)))
+                controllers[id_] = task
+            for id_ in ended_ids:
+                del controllers[id_]
+
+async def monitor_global_state(client):
+
+    subscribe_global_state = {
+        'id': '1',
+        'type': 'start',
+        'payload': {
+            'variables': {},
+            'extensions': {},
+            'operationName': None,
+            'query': SUBSCRIBE_GLOBAL_STATE
+        }
+    }
+
+    async with client.websocket_connect("/") as ws:
+        await ws.send_json(subscribe_global_state)
+        while True:
+            resp_json = await ws.receive_json()
+            if resp_json['type'] == 'complete':
+                break
+            print(resp_json['payload']['data']['globalState'])
+            if resp_json['payload']['data']['globalState'] == 'finished':
+                return
+
+@pytest.mark.asyncio
+async def test_run(snapshot):
+
+    query_global_state = { 'query': QUERY_GLOBAL_STATE }
 
     mutate_exec = { 'query': MUTATE_EXEC }
-
-    mutate_send_pdb_command = {
-        'query': MUTATE_SEND_PDB_COMMAND
-    }
 
     headers = {
         'Content-Type:': "application/json"
     }
 
     async with TestClient(app) as client:
-        resp = await client.post("/", json=query_state, headers=headers)
-        assert 'initialized' == resp.json()['data']['state']['globalState']
-
         resp = await client.post("/", json=query_global_state, headers=headers)
         assert 'initialized' == resp.json()['data']['globalState']
 
-        async with client.websocket_connect("/") as ws_global_state, \
-                   client.websocket_connect("/") as ws_thread_task_ids, \
-                   client.websocket_connect("/") as ws:
+        task_monitor_global_state = asyncio.create_task(monitor_global_state(client))
 
-            await ws_global_state.send_json(subscribe_global_state)
-            resp_json = await ws_global_state.receive_json()
-            assert 'initialized' == resp_json['payload']['data']['globalState']
+        resp = await client.post("/", json=mutate_exec, headers=headers)
+        assert resp.json()['data']['exec']
 
-            await ws_thread_task_ids.send_json(subscribe_thread_task_ids)
-            # print('here')
-            # resp_json = await ws_thread_task_ids.receive_json()
-            # print('here')
-            # from pprint import pprint
-            # pprint(resp_json['payload']['data'])
+        task_control_execution = asyncio.create_task(control_execution(client))
 
-            await ws.send_json(subscribe_state)
-            # resp_json = await ws.receive_json()
-            # state = resp_json['payload']['data']['state']
-            # assert state['globalState'] == 'initialized'
-
-            resp = await client.post("/", json=mutate_exec, headers=headers)
-            assert resp.json()['data']['exec']
-
-            resp_json = await ws_global_state.receive_json()
-            assert 'running' == resp_json['payload']['data']['globalState']
-
-            niterations = 0
-            all_thread_tasks = set()
-            async for thread_tasks in prompting_thread_tasks(ws):
-                niterations += 1
-                all_thread_tasks.update(thread_tasks)
-                for thread_id, task_id in thread_tasks:
-                    mutate_send_pdb_command['variables'] = {
-                        'threadId': thread_id,
-                        'taskId': task_id,
-                        'command': 'continue'
-                    }
-                    resp = await client.post("/", json=mutate_send_pdb_command, headers=headers)
-            assert niterations >= 4
-            assert len(all_thread_tasks) == 5
-
-            resp_json = await ws_global_state.receive_json()
-            assert 'running' == resp_json['payload']['data']['globalState']
-
-        resp = await client.post("/", json=query_state, headers=headers)
-        assert 'finished' == resp.json()['data']['state']['globalState']
+        await task_monitor_global_state
 
         resp = await client.post("/", json=query_global_state, headers=headers)
         assert 'finished' == resp.json()['data']['globalState']
