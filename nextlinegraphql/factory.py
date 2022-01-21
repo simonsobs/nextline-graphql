919b75ff8d5dff743c1da2193a6c6b202c7bc2a2
Tai Sakuma <tai.sakuma@gmail.com>
2022-01-21 17:05:23 -0500

    move create_app() to factory.py

---
+++ nextlinegraphql/factory.py
@@ -0,0 +1,51 @@
+from ariadne.asgi import GraphQL
+
+from starlette.applications import Starlette
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
+
+from .schema import schema
+
+
+##__________________________________________________________________||
+class WGraphQL(GraphQL):
+    """Wrap GraphQL for introspection"""
+
+    async def __call__(self, scope, receive, send):
+        # print(scope)
+        # print(receive)
+        # print(send)
+        await super().__call__(scope, receive, send)
+
+    async def handle_websocket_message(
+        self, message, websocket, subscriptions
+    ):
+        # print(message)
+        # print(websocket)
+        # print(subscriptions)
+        await super().handle_websocket_message(
+            message, websocket, subscriptions
+        )
+
+
+def create_app():
+
+    # app_ = GraphQL(schema, debug=True)
+    app_ = WGraphQL(schema, debug=True)
+
+    middleware = [
+        Middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
+    ]
+
+    app = Starlette(debug=True, middleware=middleware)
+    app.mount("/", app_)
+
+    return app
+
+
+##__________________________________________________________________||
