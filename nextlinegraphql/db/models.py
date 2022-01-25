ac7b0040a77894552db74b9fe474828d4f7f91ef
Tai Sakuma <tai.sakuma@gmail.com>
2022-01-25 14:49:33 -0500

    add a model StateChange

--- nextlinegraphql/db/models.py
+++ nextlinegraphql/db/models.py
@@ -1,6 +1,7 @@
+import datetime
 from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
 
 
 Base = declarative_base()
@@ -10,3 +11,10 @@ class Hello(Base):
     __tablename__ = "hello"
     id = Column(Integer, primary_key=True, index=True)
     message = Column(String)
+
+
+class StateChange(Base):
+    __tablename__ = "state_change_log"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    time = Column(DateTime(), default=lambda: datetime.datetime.now())
