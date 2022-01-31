861e617a6d794466770881b43d7f5547a0adc1a1
Tai Sakuma <tai.sakuma@gmail.com>
2022-01-31 12:04:44 -0500

    update docstring

--- tests/test_version.py
+++ tests/test_version.py
@@ -3,7 +3,7 @@ import nextlinegraphql
 
 ##__________________________________________________________________||
 def test_version():
-    """test if the version string is attached to the module"""
+    """Confirm that the version string is attached to the module"""
     nextlinegraphql.__version__
 
 
