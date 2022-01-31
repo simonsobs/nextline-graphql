07537a437624bc0e9ce0c4f2d6d38c17b0800d41
Tai Sakuma <tai.sakuma@gmail.com>
2022-01-31 15:44:35 -0500

    clean code

--- nextlinegraphql/nl/__init__.py
+++ nextlinegraphql/nl/__init__.py
@@ -1,22 +1,8 @@
-import sys
 import asyncio
-
-from pathlib import Path
 
 from nextline import Nextline
 
-THIS_DIR = Path(__file__).resolve().parent
-SCRIPT_DIR = THIS_DIR.joinpath("example_script")
-SCRIPT_PATH = SCRIPT_DIR.joinpath("script.py")
-
-sys.path.insert(0, str(SCRIPT_DIR))
-
-with open(SCRIPT_PATH) as f:
-    statement = f.read()
-
-del SCRIPT_PATH
-del SCRIPT_DIR
-del THIS_DIR
+from .example_script import statement
 
 
 nextline_holder = []
---
+++ nextlinegraphql/nl/example_script/__init__.py
@@ -0,0 +1,12 @@
+import sys
+from pathlib import Path
+
+__all__ = ["statement"]
+
+THIS_DIR = Path(__file__).resolve().parent
+SCRIPT_PATH = THIS_DIR.joinpath("script.py")
+
+sys.path.insert(0, str(THIS_DIR))
+
+with open(SCRIPT_PATH) as f:
+    statement = f.read()
