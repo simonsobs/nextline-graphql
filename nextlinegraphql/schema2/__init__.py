d66ab0bddafb24ace8202b0fdfa94276b3bb904b
Tai Sakuma <tai.sakuma@gmail.com>
2022-03-07 13:42:17 -0500

    implement query "source"

--- nextlinegraphql/schema2/__init__.py
+++ nextlinegraphql/schema2/__init__.py
@@ -1,11 +1,22 @@
+from __future__ import annotations
+
 import strawberry
+from strawberry.types import Info
+
+from typing import TYPE_CHECKING, List, Optional
+
+if TYPE_CHECKING:
+    from nextline import Nextline
+
+
+def resolve_source(info: Info, file_name: Optional[str] = None) -> List[str]:
+    nextline: Nextline = info.context["nextline"]
+    return nextline.get_source(file_name)
 
 
 @strawberry.type
 class Query:
-    @strawberry.field
-    def hello(self) -> str:
-        return "world"
+    source: List[str] = strawberry.field(resolver=resolve_source)
 
 
 schema = strawberry.Schema(query=Query)
