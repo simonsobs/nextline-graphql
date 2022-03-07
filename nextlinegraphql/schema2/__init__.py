8708669a451e362e4f03d375dbc51acc07bac885
Tai Sakuma <tai.sakuma@gmail.com>
2022-03-07 14:44:38 -0500

    use alias for type hint

--- nextlinegraphql/schema2/__init__.py
+++ nextlinegraphql/schema2/__init__.py
@@ -8,6 +8,8 @@ from typing import TYPE_CHECKING, AsyncGenerator, List, Optional
 if TYPE_CHECKING:
     from nextline import Nextline
 
+AGen = AsyncGenerator
+
 
 def resolve_source(info: Info, file_name: Optional[str] = None) -> List[str]:
     nextline: Nextline = info.context["nextline"]
@@ -19,30 +21,30 @@ class Query:
     source: List[str] = strawberry.field(resolver=resolve_source)
 
 
-def subscribe_global_state(info: Info) -> AsyncGenerator[str, None]:
+def subscribe_global_state(info: Info) -> AGen[str, None]:
     nextline: Nextline = info.context["nextline"]
     return nextline.subscribe_state()
 
 
-def subscribe_run_no(info: Info) -> AsyncGenerator[int, None]:
+def subscribe_run_no(info: Info) -> AGen[int, None]:
     nextline: Nextline = info.context["nextline"]
     return nextline.subscribe_run_no()
 
 
-def subscribe_trace_ids(info: Info) -> AsyncGenerator[List[int], None]:
+def subscribe_trace_ids(info: Info) -> AGen[List[int], None]:
     nextline: Nextline = info.context["nextline"]
     return nextline.subscribe_trace_ids()
 
 
 @strawberry.type
 class Subscription:
-    global_state: AsyncGenerator[str, None] = strawberry.field(
+    global_state: AGen[str, None] = strawberry.field(
         is_subscription=True, resolver=subscribe_global_state
     )
-    run_no: AsyncGenerator[str, None] = strawberry.field(
+    run_no: AGen[str, None] = strawberry.field(
         is_subscription=True, resolver=subscribe_run_no
     )
-    trace_ids: AsyncGenerator[List[int], None] = strawberry.field(
+    trace_ids: AGen[List[int], None] = strawberry.field(
         is_subscription=True, resolver=subscribe_trace_ids
     )
 
