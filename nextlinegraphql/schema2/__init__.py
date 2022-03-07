2961f787d004cd82358b8250cced07affcbfedb6
Tai Sakuma <tai.sakuma@gmail.com>
2022-03-07 15:32:49 -0500

    implement queries

--- nextlinegraphql/schema2/__init__.py
+++ nextlinegraphql/schema2/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import dataclasses
+import traceback
 
 import strawberry
 from strawberry.types import Info
@@ -13,14 +14,44 @@ if TYPE_CHECKING:
 AGen = AsyncGenerator
 
 
+def resolve_global_state(info: Info) -> str:
+    nextline: Nextline = info.context["nextline"]
+    return nextline.state
+
+
+def resolve_run_no(info: Info) -> int:
+    nextline: Nextline = info.context["nextline"]
+    return nextline.run_no
+
+
 def resolve_source(info: Info, file_name: Optional[str] = None) -> List[str]:
     nextline: Nextline = info.context["nextline"]
     return nextline.get_source(file_name)
 
 
+def resolve_source_line(
+    info: Info, line_no: int, file_name: Optional[str]
+) -> str:
+    nextline: Nextline = info.context["nextline"]
+    return nextline.get_source_line(line_no, file_name)
+
+
+def resolve_exception(info: Info) -> Optional[str]:
+    nextline: Nextline = info.context["nextline"]
+    if exc := nextline.exception():
+        return "".join(
+            traceback.format_exception(type(exc), exc, exc.__traceback__)
+        )
+    return None
+
+
 @strawberry.type
 class Query:
+    global_state: str = strawberry.field(resolver=resolve_global_state)
+    run_no: int = strawberry.field(resolver=resolve_run_no)
     source: List[str] = strawberry.field(resolver=resolve_source)
+    source_line: str = strawberry.field(resolver=resolve_source_line)
+    exception: Optional[str] = strawberry.field(resolver=resolve_exception)
 
 
 def subscribe_global_state(info: Info) -> AGen[str, None]:
