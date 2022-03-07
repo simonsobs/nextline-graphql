8ff1aaec5cca7f7bc27f96735485db8c5248601d
Tai Sakuma <tai.sakuma@gmail.com>
2022-03-07 14:01:10 -0500

    implement subscription global_state

--- nextlinegraphql/schema2/__init__.py
+++ nextlinegraphql/schema2/__init__.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import strawberry
 from strawberry.types import Info
 
-from typing import TYPE_CHECKING, List, Optional
+from typing import TYPE_CHECKING, AsyncGenerator, List, Optional
 
 if TYPE_CHECKING:
     from nextline import Nextline
@@ -19,4 +19,16 @@ class Query:
     source: List[str] = strawberry.field(resolver=resolve_source)
 
 
-schema = strawberry.Schema(query=Query)
+def global_state_generator(info: Info) -> AsyncGenerator[str, None]:
+    nextline: Nextline = info.context["nextline"]
+    return nextline.subscribe_state()
+
+
+@strawberry.type
+class Subscription:
+    global_state: AsyncGenerator[str, None] = strawberry.field(
+        is_subscription=True, resolver=global_state_generator
+    )
+
+
+schema = strawberry.Schema(query=Query, subscription=Subscription)
