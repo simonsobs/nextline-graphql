a7e57e47983e72d455625c670fda8b7c8cec5d4f
Tai Sakuma <tai.sakuma@gmail.com>
2022-03-09 15:49:28 -0500

    update test

--- tests/schema/queries/test_exception.py
+++ tests/schema/queries/test_exception.py
@@ -3,7 +3,7 @@ from async_asgi_testclient import TestClient
 
 import pytest
 
-from typing import List, Set
+from typing import Set
 
 from nextline.utils import agen_with_wait
 
@@ -11,9 +11,7 @@ from ..funcs import gql_request, gql_subscribe
 
 from ..graphql import (
     QUERY_STATE,
-    QUERY_SOURCE_LINE,
     QUERY_EXCEPTION,
-    SUBSCRIBE_STATE,
     SUBSCRIBE_TRACE_IDS,
     SUBSCRIBE_PROMPTING,
     MUTATE_RESET,
@@ -22,22 +20,23 @@ from ..graphql import (
 )
 
 
-SOURCE_ONE = """
-import time
-time.sleep(0.1)
-""".strip()
-
 SOURCE_RAISE = """
 raise Exception('foo', 'bar')
 """.strip()
 
 
 @pytest.mark.asyncio
-async def test_run(client: TestClient):
+async def test_one(client: TestClient):
 
-    task_subscribe_state = asyncio.create_task(subscribe_state(client))
+    statement = SOURCE_RAISE
+    await run_statement(client, statement)
 
-    variables = {"statement": SOURCE_RAISE}
+    data = await gql_request(client, QUERY_EXCEPTION)
+    assert "('foo', 'bar')" in data["exception"]
+
+
+async def run_statement(client, statement: str):
+    variables = {"statement": statement}
     data = await gql_request(client, MUTATE_RESET, variables=variables)
     assert data["reset"]
 
@@ -53,27 +52,10 @@ async def test_run(client: TestClient):
     data = await gql_request(client, MUTATE_EXEC)
     assert data["exec"]
 
-    states, *_ = await asyncio.gather(
-        task_subscribe_state,
-        task_control_execution,
-    )
-    assert ["initialized", "running", "exited", "finished"] == states
+    await task_control_execution
 
     data = await gql_request(client, QUERY_STATE)
     assert "finished" == data["state"]
-
-    data = await gql_request(client, QUERY_EXCEPTION)
-    assert "(\'foo\', \'bar\')" in data["exception"]
-
-
-async def subscribe_state(client: TestClient) -> List[str]:
-    ret = []
-    async for data in gql_subscribe(client, SUBSCRIBE_STATE):
-        s = data["state"]
-        ret.append(s)
-        if s == "finished":
-            break
-    return ret
 
 
 async def control_execution(client: TestClient):
@@ -99,8 +81,6 @@ async def control_execution(client: TestClient):
 async def control_trace(client: TestClient, trace_id: int) -> None:
     # print(f'control_trace({trace_id})')
 
-    to_step = ["script_threading.run()", "script_asyncio.run()"]
-
     async for data in gql_subscribe(
         client,
         SUBSCRIBE_PROMPTING,
@@ -109,29 +89,11 @@ async def control_trace(client: TestClient, trace_id: int) -> None:
         state = data["prompting"]
         # print(state)
         if state["prompting"]:
-            command = "next"
-            if state["traceEvent"] == "line":
-                data = await gql_request(
-                    client,
-                    QUERY_SOURCE_LINE,
-                    variables={
-                        "lineNo": state["lineNo"],
-                        "fileName": state["fileName"],
-                    },
-                )
-                source_line = data["sourceLine"]
-
-                # print(source_line)
-                # print(source_line in to_step)
-                if source_line in to_step:
-                    command = "step"
-
+            command = "continue"
+            variables = {"traceId": trace_id, "command": command}
             data = await gql_request(
                 client,
                 MUTATE_SEND_PDB_COMMAND,
-                variables={
-                    "traceId": trace_id,
-                    "command": command,
-                },
+                variables=variables,
             )
             assert data["sendPdbCommand"]
