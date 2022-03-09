1fedb61a15267a8d61c034a842289a68924e3f57
Tai Sakuma <tai.sakuma@gmail.com>
2022-03-09 15:59:52 -0500

    move functions to a new module

---
+++ tests/schema/queries/funcs.py
@@ -0,0 +1,80 @@
+import asyncio
+from typing import Set
+from async_asgi_testclient import TestClient
+
+from nextline.utils import agen_with_wait
+
+from ..funcs import gql_request, gql_subscribe
+
+from ..graphql import (
+    QUERY_STATE,
+    SUBSCRIBE_TRACE_IDS,
+    SUBSCRIBE_PROMPTING,
+    MUTATE_RESET,
+    MUTATE_EXEC,
+    MUTATE_SEND_PDB_COMMAND,
+)
+
+
+async def run_statement(client, statement: str):
+    variables = {"statement": statement}
+    data = await gql_request(client, MUTATE_RESET, variables=variables)
+    assert data["reset"]
+
+    data = await gql_request(client, QUERY_STATE)
+    assert "initialized" == data["state"]
+
+    await asyncio.sleep(0.01)
+
+    task_control_execution = asyncio.create_task(control_execution(client))
+
+    await asyncio.sleep(0.01)
+
+    data = await gql_request(client, MUTATE_EXEC)
+    assert data["exec"]
+
+    await task_control_execution
+
+    data = await gql_request(client, QUERY_STATE)
+    assert "finished" == data["state"]
+
+
+async def control_execution(client: TestClient):
+
+    agen = agen_with_wait(gql_subscribe(client, SUBSCRIBE_TRACE_IDS))
+
+    prev_ids: Set[int] = set()
+    async for data in agen:
+        if not (ids := set(data["traceIds"])):
+            break
+        new_ids, prev_ids = ids - prev_ids, ids
+        tasks = {
+            asyncio.create_task(
+                control_trace(client, id_),
+            )
+            for id_ in new_ids
+        }
+        _, pending = await agen.asend(tasks)
+
+    await asyncio.gather(*pending)
+
+
+async def control_trace(client: TestClient, trace_id: int) -> None:
+    # print(f'control_trace({trace_id})')
+
+    async for data in gql_subscribe(
+        client,
+        SUBSCRIBE_PROMPTING,
+        variables={"traceId": trace_id},
+    ):
+        state = data["prompting"]
+        # print(state)
+        if state["prompting"]:
+            command = "continue"
+            variables = {"traceId": trace_id, "command": command}
+            data = await gql_request(
+                client,
+                MUTATE_SEND_PDB_COMMAND,
+                variables=variables,
+            )
+            assert data["sendPdbCommand"]
--- tests/schema/queries/test_exception.py
+++ tests/schema/queries/test_exception.py
@@ -1,24 +1,10 @@
-import asyncio
+import pytest
 from async_asgi_testclient import TestClient
 
-import pytest
+from ..funcs import gql_request
+from ..graphql import QUERY_EXCEPTION
 
-from typing import Set
-
-from nextline.utils import agen_with_wait
-
-from ..funcs import gql_request, gql_subscribe
-
-from ..graphql import (
-    QUERY_STATE,
-    QUERY_EXCEPTION,
-    SUBSCRIBE_TRACE_IDS,
-    SUBSCRIBE_PROMPTING,
-    MUTATE_RESET,
-    MUTATE_EXEC,
-    MUTATE_SEND_PDB_COMMAND,
-)
-
+from .funcs import run_statement
 
 SOURCE_RAISE = """
 raise Exception('foo', 'bar')
@@ -33,67 +19,3 @@ async def test_one(client: TestClient):
 
     data = await gql_request(client, QUERY_EXCEPTION)
     assert "('foo', 'bar')" in data["exception"]
-
-
-async def run_statement(client, statement: str):
-    variables = {"statement": statement}
-    data = await gql_request(client, MUTATE_RESET, variables=variables)
-    assert data["reset"]
-
-    data = await gql_request(client, QUERY_STATE)
-    assert "initialized" == data["state"]
-
-    await asyncio.sleep(0.01)
-
-    task_control_execution = asyncio.create_task(control_execution(client))
-
-    await asyncio.sleep(0.01)
-
-    data = await gql_request(client, MUTATE_EXEC)
-    assert data["exec"]
-
-    await task_control_execution
-
-    data = await gql_request(client, QUERY_STATE)
-    assert "finished" == data["state"]
-
-
-async def control_execution(client: TestClient):
-
-    agen = agen_with_wait(gql_subscribe(client, SUBSCRIBE_TRACE_IDS))
-
-    prev_ids: Set[int] = set()
-    async for data in agen:
-        if not (ids := set(data["traceIds"])):
-            break
-        new_ids, prev_ids = ids - prev_ids, ids
-        tasks = {
-            asyncio.create_task(
-                control_trace(client, id_),
-            )
-            for id_ in new_ids
-        }
-        _, pending = await agen.asend(tasks)
-
-    await asyncio.gather(*pending)
-
-
-async def control_trace(client: TestClient, trace_id: int) -> None:
-    # print(f'control_trace({trace_id})')
-
-    async for data in gql_subscribe(
-        client,
-        SUBSCRIBE_PROMPTING,
-        variables={"traceId": trace_id},
-    ):
-        state = data["prompting"]
-        # print(state)
-        if state["prompting"]:
-            command = "continue"
-            variables = {"traceId": trace_id, "command": command}
-            data = await gql_request(
-                client,
-                MUTATE_SEND_PDB_COMMAND,
-                variables=variables,
-            )
-            assert data["sendPdbCommand"]
