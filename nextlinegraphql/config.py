34fc668fefabc5166d50f64664ca4152a4b778bb
Tai Sakuma <tai.sakuma@gmail.com>
2022-03-13 21:23:33 -0400

    update config path

--- nextlinegraphql/config.py
+++ nextlinegraphql/config.py
@@ -7,12 +7,13 @@ from dynaconf import Dynaconf, Validator
 from pathlib import Path
 
 here = Path(__file__).resolve().parent
+cwd = Path.cwd()
 
 settings = Dynaconf(
     envvar_prefix="NEXTLINE",
     settings_files=[
         str(here.joinpath("config", "default.toml")),
-        "nextline-graphql.toml"
+        str(cwd.joinpath("nextline-graphql.toml")),
     ],
 )
 
