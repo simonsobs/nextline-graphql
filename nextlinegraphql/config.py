8fafd32632a99e43f0a63a229ff83f5d226db32b
Tai Sakuma <tai.sakuma@gmail.com>
2022-03-13 20:18:31 -0400

    reorganize dynaconf files. add a setting

--- nextlinegraphql/config.py
+++ nextlinegraphql/config.py
@@ -1,6 +1,21 @@
-from dynaconf import Dynaconf
+"""Configuration by Dynaconf
+
+Example: https://github.com/rochacbruno/learndynaconf/blob/main/config.py
+"""
+
+from dynaconf import Dynaconf, Validator
+from pathlib import Path
+
+cwd = Path(__file__).resolve().parent
+default_settings_file = cwd.joinpath("config", "default.toml")
 
 settings = Dynaconf(
     envvar_prefix="NEXTLINE",
-    settings_files=["settings.toml"],
+    settings_files=[str(default_settings_file)],
 )
+
+settings.validators.register(
+    Validator("DB.URL", must_exist=True, is_type_of=str),
+)
+
+settings.validators.validate()
