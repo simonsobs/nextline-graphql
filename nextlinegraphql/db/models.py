7ee1fa97a1404adea62bf8f26919306075576899
Tai Sakuma <tai.sakuma@gmail.com>
2022-03-16 19:08:52 -0400

    add naming convention of constraints in SQLAlchemy database

--- nextlinegraphql/db/models.py
+++ nextlinegraphql/db/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from sqlalchemy import MetaData
 
 from sqlalchemy import (
     Column,
@@ -12,8 +13,19 @@ from sqlalchemy import (
     UniqueConstraint,
 )
 
+# https://docs.sqlalchemy.org/en/14/core/constraints.html#configuring-a-naming-convention-for-a-metadata-collection
+# https://github.com/simonsobs/acondbs/blob/7b4e5ab967ce/acondbs/db/sa.py
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    # "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+metadata = MetaData(naming_convention=convention)
 
-Base = declarative_base()
+Base = declarative_base(metadata=metadata)
 
 
 class Hello(Base):
