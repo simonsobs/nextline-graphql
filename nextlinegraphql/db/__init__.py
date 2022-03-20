b5984f130b6143b6a085c2ee0febef51147a1d8f
Tai Sakuma <tai.sakuma@gmail.com>
2022-03-19 21:28:44 -0400

    use logger

--- nextlinegraphql/db/__init__.py
+++ nextlinegraphql/db/__init__.py
@@ -2,7 +2,9 @@ from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from alembic.config import Config
+from alembic.migration import MigrationContext
 from alembic import command
+from logging import getLogger
 
 from . import models
 from .write import write_db
@@ -11,11 +13,22 @@ __all__ = ["init_db", "write_db"]
 
 
 def init_db(url: str):
+
+    logger = getLogger(__name__)
+    logger.info(f"SQLAlchemy DB URL: {url}")
+
     run_alembic_upgrade()
+
     engine = create_engine(
         url,
         connect_args={"check_same_thread": False},
     )
+
+    with engine.connect() as connection:
+        context = MigrationContext.configure(connection)
+        rev = context.get_current_revision()
+    logger.info(f"Alembic migration version: {rev!s}")
+
     models.Base.metadata.create_all(bind=engine)
     return sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -24,4 +37,6 @@ def run_alembic_upgrade():
     here = Path(__file__).resolve().parent
     config_path = here.joinpath("alembic.ini")
     config = Config(str(config_path))
+    # TODO: Arrange config so that logging doesn't need to be conditionally
+    # configured in alembic/env.py
     command.upgrade(config, "head")
