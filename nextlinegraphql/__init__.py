7287f4183ca3728648c8794c1f8401eb590118e6
Tai Sakuma <tai.sakuma@gmail.com>
2023-01-13 16:35:39 -0500

    replace versioneer with hatch_regex_commit

--- nextlinegraphql/__init__.py
+++ nextlinegraphql/__init__.py
@@ -3,30 +3,13 @@ uvicorn --factory --reload --reload-dir nextline-graphql --reload-dir nextline n
 use "--lifespan on" to show exception in lifespan
 """
 
-__all__ = ["create_app"]
+__all__ = ["create_app", "__version__"]
 
 from .factory import create_app
+from .__about__ import __version__
 
-##__________________________________________________________________||
 # remove args so that they won't be processed in the executing script
 # TODO This should be properly handled and tested
 import sys  # noqa: E402
 
 sys.argv[1:] = []
-
-##__________________________________________________________________||
-from ._version import get_versions  # noqa: E402
-
-__version__ = get_versions()["version"]
-"""str: version
-
-The version string, e.g., "0.1.2", "0.1.2+83.ga093a20.dirty".
-generated from git tags by versioneer.
-
-Versioneer: https://github.com/warner/python-versioneer
-
-"""
-
-del get_versions
-
-##__________________________________________________________________||
