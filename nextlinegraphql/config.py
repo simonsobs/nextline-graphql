2f2c6c72968e79a2a4e30d97fd22e47ace92271f
Tai Sakuma <tai.sakuma@gmail.com>
2023-01-14 10:26:54 -0500

    init dynaconf in a function instead of module level

--- nextlinegraphql/config.py
+++ nextlinegraphql/config.py
@@ -7,21 +7,30 @@ from pathlib import Path
 
 from dynaconf import Dynaconf, Validator
 
-here = Path(__file__).resolve().parent
-cwd = Path.cwd()
+HERE = Path(__file__).resolve().parent
 
-settings = Dynaconf(
-    envvar_prefix="NEXTLINE",
-    settings_files=[
-        str(here.joinpath("config", "default.toml")),
-        str(cwd.joinpath("nextline-graphql.toml")),
-        str(cwd.joinpath("migration.toml")),  # for alembic to run in ./db/
-    ],
-    environments=True,
-)
 
-settings.validators.register(
-    Validator("DB.URL", must_exist=True, is_type_of=str),
-)
+def create_settings() -> Dynaconf:
+    '''Return a Dynaconf settings after validation'''
+    cwd = Path.cwd()
 
-settings.validators.validate()
+    settings = Dynaconf(
+        envvar_prefix="NEXTLINE",
+        settings_files=[
+            str(HERE.joinpath("config", "default.toml")),
+            str(cwd.joinpath("nextline-graphql.toml")),
+            str(cwd.joinpath("migration.toml")),  # for alembic to run in ./db/
+        ],
+        environments=True,
+    )
+
+    settings.validators.register(
+        Validator("DB.URL", must_exist=True, is_type_of=str),
+    )
+
+    settings.validators.validate()
+
+    return settings
+
+
+settings = create_settings()
