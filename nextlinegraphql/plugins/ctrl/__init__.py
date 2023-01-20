22322555527c49d1470d075c526a6981f72b4ad3
Tai Sakuma <tai.sakuma@gmail.com>
2023-01-20 18:24:48 -0500

    remove a no-op method

--- nextlinegraphql/plugins/ctrl/__init__.py
+++ nextlinegraphql/plugins/ctrl/__init__.py
@@ -1,5 +1,4 @@
 __all__ = ['Plugin']
-from dynaconf import Dynaconf
 
 from nextlinegraphql import spec
 
@@ -7,10 +6,6 @@ from .schema import Mutation, Query, Subscription
 
 
 class Plugin:
-    @spec.hookimpl
-    def configure(self, settings: Dynaconf):
-        del settings
-
     @spec.hookimpl
     def schema(self):
         return (Query, Mutation, Subscription)
