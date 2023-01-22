414bbd4f481a064aa235f2705fdddcf9e33730e9
Tai Sakuma <tai.sakuma@gmail.com>
2023-01-22 11:52:01 -0500

    move config of db plugin to the plugin itself

--- nextlinegraphql/plugins/db/plugin.py
+++ nextlinegraphql/plugins/db/plugin.py
@@ -1,7 +1,8 @@
 from logging import getLogger
-from typing import Mapping
+from pathlib import Path
+from typing import Mapping, Optional, Tuple
 
-from dynaconf import Dynaconf
+from dynaconf import Dynaconf, Validator
 from nextline import Nextline
 from sqlalchemy import func, select
 from sqlalchemy.orm import Session
@@ -15,8 +16,35 @@ from .db import DB
 from .schema import Mutation, Query, Subscription
 from .write import write_db
 
+HERE = Path(__file__).resolve().parent
+DEFAULT_CONFIG_PATH = HERE / 'default.toml'
+MIGRATION_CONFIG_PATH = HERE / 'migration.toml'
+
+assert DEFAULT_CONFIG_PATH.is_file()
+assert MIGRATION_CONFIG_PATH.is_file()
+
+PRELOAD = (str(DEFAULT_CONFIG_PATH),)
+SETTINGS = ()
+VALIDATORS = (Validator("DB.URL", must_exist=True, is_type_of=str),)
+
+SETTINGS_FOR_MIGRATION = (str(MIGRATION_CONFIG_PATH),)
+
 
 class Plugin:
+    @spec.hookimpl
+    def dynaconf_preload(self) -> Optional[Tuple[str, ...]]:
+        return PRELOAD
+
+    @spec.hookimpl
+    def dynaconf_settings_files(self) -> Optional[Tuple[str, ...]]:
+        if Path.cwd() == HERE:
+            return SETTINGS_FOR_MIGRATION
+        return SETTINGS
+
+    @spec.hookimpl
+    def dynaconf_validators(self) -> Optional[Tuple[Validator, ...]]:
+        return VALIDATORS
+
     @spec.hookimpl
     def configure(self, settings: Dynaconf):
         self._db = DB(settings.db['url'])
