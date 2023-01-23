50981848239af0e6ff9f55dd66128c0e889873df
Tai Sakuma <tai.sakuma@gmail.com>
2023-01-23 15:47:07 -0500

    instantiate Nextline in a plugin

--- nextlinegraphql/factory.py
+++ nextlinegraphql/factory.py
@@ -1,8 +1,7 @@
 import contextlib
-from typing import Optional
+from typing import Dict, Optional
 
 from dynaconf import Dynaconf
-from nextline import Nextline
 from starlette.applications import Starlette
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
@@ -10,7 +9,6 @@ from starlette.middleware.cors import CORSMiddleware
 from . import graphql
 from .config import load_settings
 from .custom.pluggy import PluginManager
-from .example_script import statement
 from .hook import load_plugins
 from .logging import configure_logging
 
@@ -27,20 +25,14 @@ def create_app(config: Optional[Dynaconf] = None):
 
     configure(hook=hook, config=config)
 
-    run_no: int = max(hook.hook.initial_run_no(), default=1)
-    script: str = [*hook.hook.initial_script(), statement][0]
-
-    nextline = Nextline(script, run_no)
-
     app_ = graphql.create_app(hook=hook)
 
     @contextlib.asynccontextmanager
     async def lifespan(app: Starlette):
-        assert nextline
 
         app.mount('/', app_)
 
-        context = {'nextline': nextline}
+        context: Dict = {}
         hook.hook.update_lifespan_context(app=app, context=context)
 
         async with hook.awith.lifespan(app=app, context=context):
--- nextlinegraphql/plugins/ctrl/__init__.py
+++ nextlinegraphql/plugins/ctrl/__init__.py
@@ -1,10 +1,12 @@
 __all__ = ['Plugin']
 
-from typing import Mapping, MutableMapping
+from typing import MutableMapping
 
-from starlette.applications import Starlette
+from nextline import Nextline
 
+from nextlinegraphql.custom import pluggy
 from nextlinegraphql.custom.decorator import asynccontextmanager
+from nextlinegraphql.example_script import statement
 from nextlinegraphql.hook import spec
 
 from .schema import Mutation, Query, Subscription
@@ -12,14 +14,23 @@ from .schema import Mutation, Query, Subscription
 
 class Plugin:
     @spec.hookimpl
+    def configure(self, hook: pluggy.PluginManager) -> None:
+        run_no: int = max(hook.hook.initial_run_no(), default=1)
+        script: str = [*hook.hook.initial_script(), statement][0]
+        self._nextline = Nextline(script, run_no)
+
+    @spec.hookimpl
     def schema(self):
         return (Query, Mutation, Subscription)
 
+    @spec.hookimpl
+    def update_lifespan_context(self, context: MutableMapping) -> None:
+        context['nextline'] = self._nextline
+
     @spec.hookimpl(trylast=True)  # trylast so to be the innermost context
     @asynccontextmanager
-    async def lifespan(self, app: Starlette, context: Mapping):
+    async def lifespan(self):
         '''Yield within the nextline context.'''
-        self._nextline = context['nextline']
         async with self._nextline:
             yield
 
