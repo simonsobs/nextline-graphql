3f823052e82de5952d00ea1a63bfec0cb232aec8
Tai Sakuma <tai.sakuma@gmail.com>
2023-01-23 16:59:04 -0500

    clean code, add docstring

--- nextlinegraphql/factory.py
+++ nextlinegraphql/factory.py
@@ -11,21 +11,24 @@ from .hook import load_plugins
 
 
 def create_app() -> Starlette:
+    '''App factory for Uvicorn
+
+    Use the factory option to run.
+
+    $ uvicorn --factory nextlinegraphql:create_app
+
+    Uvicorn Doc: https://www.uvicorn.org/#application-factories
+    '''
 
     hook = load_plugins()
-
     config = load_settings(hook)
-
     hook.hook.configure(settings=config, hook=hook)
-
     configure_logging(config.logging)
 
     @contextlib.asynccontextmanager
     async def lifespan(app: Starlette):
-
         context: Dict = {}
         hook.hook.update_lifespan_context(app=app, context=context)
-
         async with hook.awith.lifespan(app=app, context=context):
             yield
 
