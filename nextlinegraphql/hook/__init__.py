3515e7bcf706cb5b14b3de8c72785a16de9da3ec
Tai Sakuma <tai.sakuma@gmail.com>
2023-01-23 13:28:37 -0500

    add a comment

--- nextlinegraphql/hook/__init__.py
+++ nextlinegraphql/hook/__init__.py
@@ -8,9 +8,14 @@ from . import spec
 
 def initialize_plugins() -> PluginManager:
     '''Return a pluggy PluginManager with the plugins registered'''
+
     pm = PluginManager(spec.PROJECT_NAME)
     pm.add_hookspecs(spec)
+
+    # The hooks are called in the reverse order of the plugin registration.
+    # https://pluggy.readthedocs.io/en/stable/#call-time-order
     pm.load_setuptools_entrypoints(spec.PROJECT_NAME)
     pm.register(db.Plugin())
     pm.register(ctrl.Plugin())
+
     return pm
