e70a3b2cce776e7dd89aa66cbcc0b85c9a6ef8f3
Tai Sakuma <tai.sakuma@gmail.com>
2023-03-30 10:33:57 -0400

    add type hints

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,31 +1,33 @@
 import asyncio
 import sys
 import threading
+from typing import AsyncIterator, Iterator
 
 import pytest
 from async_asgi_testclient import TestClient
+from starlette.applications import Starlette
 
 from nextlinegraphql import create_app
 
 
 @pytest.fixture(autouse=True)
-def recover_trace():
+def recover_trace() -> Iterator[None]:
     """Set the original trace function back after each test"""
     trace_org = sys.gettrace()
     yield
     sys.settrace(trace_org)
-    threading.settrace(trace_org)
+    threading.settrace(trace_org)  # type: ignore
 
 
 @pytest.fixture
-async def client(app):
+async def client(app: Starlette) -> AsyncIterator[TestClient]:
     async with TestClient(app) as y:
         await asyncio.sleep(0)
         yield y
 
 
 @pytest.fixture
-def app():
+def app() -> Starlette:
     return create_app()
 
 
