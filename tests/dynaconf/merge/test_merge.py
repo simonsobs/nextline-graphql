00b2b447ac5c34012984892a23a377e18a184add
Tai Sakuma <tai.sakuma@gmail.com>
2023-03-31 17:56:56 -0400

    format source

--- tests/dynaconf/merge/test_merge.py
+++ tests/dynaconf/merge/test_merge.py
@@ -5,7 +5,7 @@ from pytest import FixtureRequest, MonkeyPatch, fixture
 
 
 def test_merge(config: Dynaconf, envvar_url: bool, envvar_connect_args: bool):
-    """Confirm how merging works
+    '''Confirm how merging works
 
     https://www.dynaconf.com/merging/
 
@@ -15,30 +15,30 @@ def test_merge(config: Dynaconf, envvar_url: bool, envvar_connect_args: bool):
     Note that the check_same_thread option can be provided as part of url.
     https://docs.sqlalchemy.org/en/14/dialects/sqlite.html#uri-connections
 
-    """
+    '''
     if envvar_url:
-        assert "sqlite:///env.sqlite3" == config.db.url
+        assert 'sqlite:///env.sqlite3' == config.db.url
     else:
-        assert "sqlite:///config.sqlite3" == config.db.url
+        assert 'sqlite:///config.sqlite3' == config.db.url
 
     if envvar_connect_args:
-        assert {"check_same_thread": True} == config.db.connect_args
+        assert {'check_same_thread': True} == config.db.connect_args
     elif envvar_connect_args is None:
         assert {} == config.db.connect_args
     else:
-        assert {"check_same_thread": False} == config.db.connect_args
+        assert {'check_same_thread': False} == config.db.connect_args
 
 
 @fixture
 def config():
     here = Path(__file__).resolve().parent
     ret = Dynaconf(
-        envvar_prefix="NEXTLINE",
+        envvar_prefix='NEXTLINE',
         settings_files=[
             str(here / 'default.toml'),
             str(here / 'config.toml'),
         ],
-        # environments=True,  # enable sections, e.g., "development"
+        # environments=True,  # enable sections, e.g., 'development'
     )
     return ret
 
@@ -46,7 +46,7 @@ def config():
 @fixture(params=[True, False])
 def envvar_url(request: FixtureRequest, monkeypatch: MonkeyPatch):
     if ret := request.param:
-        monkeypatch.setenv("NEXTLINE_DB__URL", "sqlite:///env.sqlite3")
+        monkeypatch.setenv('NEXTLINE_DB__URL', 'sqlite:///env.sqlite3')
         # dunder merging
         # https://www.dynaconf.com/merging/#dunder-merging-for-nested-structures
     return ret
@@ -59,12 +59,12 @@ def envvar_connect_args(request: FixtureRequest, monkeypatch: MonkeyPatch):
     # None: remove
     if ret := request.param:
         monkeypatch.setenv(
-            "NEXTLINE_DB__CONNECT_ARGS",
-            "{check_same_thread=true}",
+            'NEXTLINE_DB__CONNECT_ARGS',
+            '{check_same_thread=true}',
         )
     elif ret is None:
         monkeypatch.setenv(
-            "NEXTLINE_DB__connect_args__check_same_thread",
-            "@del",
+            'NEXTLINE_DB__connect_args__check_same_thread',
+            '@del',
         )
     return ret
