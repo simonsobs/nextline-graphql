0564398b4a8f94d7e170b62b9e57d76acbfd5c77
Tai Sakuma <tai.sakuma@gmail.com>
2024-05-13 19:14:57 -0400

    Update tests

--- tests/plugins/graphql/schema/queries/test_settings.py
+++ tests/plugins/graphql/schema/queries/test_settings.py
@@ -1,9 +1,20 @@
 import json
 
+from strawberry import Schema
+
+from nextlinegraphql.config import load_settings
+from nextlinegraphql.hook import load_plugins
 from nextlinegraphql.plugins.graphql.graphql import QUERY_SETTINGS
-from nextlinegraphql.plugins.graphql.test import TestClient, gql_request
+from nextlinegraphql.plugins.graphql.schema import Query
 
 
-async def test_settings(client: TestClient):
-    data = await gql_request(client, QUERY_SETTINGS)
-    assert json.loads(data['settings'])
+async def test_settings() -> None:
+    schema = Schema(query=Query)
+    hook = load_plugins()
+    settings = load_settings(hook)
+    context = {'settings': settings}
+    result = await schema.execute(QUERY_SETTINGS, context_value=context)
+    assert not result.errors
+    assert result.data
+    assert result.data['settings']
+    assert json.dumps(settings.to_dict()) == result.data['settings']
--- tests/plugins/graphql/test_graphiql.py
+++ tests/plugins/graphql/test_graphiql.py
@@ -2,9 +2,9 @@ from nextlinegraphql.plugins.graphql.test import TestClient
 
 
 async def test_one(client: TestClient):
-    """Assert GraphQL IDE for the HTTP get request"""
-    headers = {"Accept": "text/html"}
-    resp = await client.get("/", headers=headers)
+    '''Assert GraphQL IDE for the HTTP get request'''
+    headers = {'Accept': 'text/html'}
+    resp = await client.get('/', headers=headers)
     assert resp.status_code == 200
-    assert "text/html" in resp.headers["content-type"].lower()
-    assert "graphiql" in resp.text.lower()
+    assert 'text/html' in resp.headers['content-type'].lower()
+    assert 'graphiql' in resp.text.lower()
