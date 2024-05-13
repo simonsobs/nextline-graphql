aebbf8a8916974ede929c39504c1ad3f0d13e692
Tai Sakuma <tai.sakuma@gmail.com>
2024-05-13 19:33:15 -0400

    Update a test

--- tests/plugins/graphql/schema/queries/test_settings.py
+++ tests/plugins/graphql/schema/queries/test_settings.py
@@ -1,20 +1,19 @@
 import json
 
+from dynaconf import Dynaconf
 from strawberry import Schema
 
-from nextlinegraphql.config import load_settings
-from nextlinegraphql.hook import load_plugins
 from nextlinegraphql.plugins.graphql.graphql import QUERY_SETTINGS
 from nextlinegraphql.plugins.graphql.schema import Query
 
 
 async def test_settings() -> None:
     schema = Schema(query=Query)
-    hook = load_plugins()
-    settings = load_settings(hook)
-    context = {'settings': settings}
+    settings = {'FOO': 'bar'}
+    conf = Dynaconf(**settings)
+    context = {'settings': conf}
     result = await schema.execute(QUERY_SETTINGS, context_value=context)
     assert not result.errors
     assert result.data
     assert result.data['settings']
-    assert json.dumps(settings.to_dict()) == result.data['settings']
+    assert settings == json.loads(result.data['settings'])
