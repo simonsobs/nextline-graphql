9a14eec05fc5e5f8170329e2ecb31ee32ec2ef70
Tai Sakuma <tai.sakuma@gmail.com>
2024-06-24 15:48:47 -0400

    Update gql files

--- nextlinegraphql/plugins/ctrl/graphql/__init__.py
+++ nextlinegraphql/plugins/ctrl/graphql/__init__.py
@@ -16,20 +16,28 @@ pwd = Path(__file__).resolve().parent
 
 sub = pwd / 'mutations'
 MUTATE_EXEC = read_gql(sub / 'Exec.gql')
+MUTATE_RUN_AND_CONTINUE = read_gql(sub / 'RunAndContinue.gql')
 MUTATE_RESET = read_gql(sub / 'Reset.gql')
 MUTATE_SEND_PDB_COMMAND = read_gql(sub / 'SendPdbCommand.gql')
-MUTATE_RUN_AND_CONTINUE = read_gql(sub / 'RunAndContinue.gql')
+MUTATE_INTERRUPT = read_gql(sub / 'Interrupt.gql')
+MUTATE_TERMINATE = read_gql(sub / 'Terminate.gql')
+MUTATE_KILL = read_gql(sub / 'Kill.gql')
 MUTATE_LOAD_EXAMPLE_SCRIPT = read_gql(sub / 'LoadExampleScript.gql')
 
 sub = pwd / 'queries'
 QUERY_STATE = read_gql(sub / 'State.gql')
-QUERY_EXCEPTION = read_gql(sub / 'Exception.gql')
+QUERY_RUN_NO = read_gql(sub / 'RunNo.gql')
+QUERY_TRACE_IDS = read_gql(sub / 'TraceIds.gql')
 QUERY_SOURCE = read_gql(sub / 'Source.gql')
 QUERY_SOURCE_LINE = read_gql(sub / 'SourceLine.gql')
+QUERY_EXCEPTION = read_gql(sub / 'Exception.gql')
+QUERY_CONTINUOUS_ENABLED = read_gql(sub / 'ContinuousEnabled.gql')
 
 sub = pwd / 'subscriptions'
 SUBSCRIBE_COUNTER = read_gql(sub / 'Counter.gql')
 SUBSCRIBE_STATE = read_gql(sub / 'State.gql')
-SUBSCRIBE_STDOUT = read_gql(sub / 'Stdout.gql')
+SUBSCRIBE_RUN_NO = read_gql(sub / 'RunNo.gql')
 SUBSCRIBE_TRACE_IDS = read_gql(sub / 'TraceIds.gql')
 SUBSCRIBE_PROMPTING = read_gql(sub / 'Prompting.gql')
+SUBSCRIBE_STDOUT = read_gql(sub / 'Stdout.gql')
+SUBSCRIBE_CONTINUOUS_ENABLED = read_gql(sub / 'ContinuousEnabled.gql')
