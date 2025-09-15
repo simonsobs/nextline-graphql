5f590b186320d267d92ee8211b1bdd3ec8f9103c
Tai Sakuma <tai.sakuma@gmail.com>
2025-09-15 14:13:51 -0400

    Add settings `trace_modules` and `trace_threads`

---
+++ tests/plugins/ctrl/test_config.py
@@ -0,0 +1,59 @@
+from hypothesis import HealthCheck, given, settings
+from hypothesis import strategies as st
+from pytest import MonkeyPatch
+
+from nextline_test_utils.strategies import st_none_or
+from nextlinegraphql.factory import create_hook_and_config
+from nextlinegraphql.plugins.ctrl import Plugin
+
+DEFAULT_TRACE_MODULES = False
+DEFAULT_TRACE_THREADS = False
+
+
+def test_default() -> None:
+    hook, config = create_hook_and_config(
+        enable_external_plugins=False,
+        enable_logging_configuration=False,
+        print_settings=False,
+    )
+    assert config.ctrl.trace_modules is DEFAULT_TRACE_MODULES
+    assert config.ctrl.trace_threads is DEFAULT_TRACE_THREADS
+
+    plugin = hook.get_plugin('ctrl')
+    assert isinstance(plugin, Plugin)
+    assert plugin._trace_modules is DEFAULT_TRACE_MODULES
+    assert plugin._trace_threads is DEFAULT_TRACE_THREADS
+
+
+@settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
+@given(trace_modules=st_none_or(st.booleans()), trace_threads=st_none_or(st.booleans()))
+def test_property(
+    trace_modules: bool | None, trace_threads: bool | None, monkeypatch: MonkeyPatch
+) -> None:
+    with monkeypatch.context() as m:
+        pass
+        if trace_modules is not None:
+            m.setenv('NEXTLINE_CTRL__TRACE_MODULES', str(trace_modules).lower())
+        if trace_threads is not None:
+            m.setenv('NEXTLINE_CTRL__TRACE_THREADS', str(trace_threads).lower())
+
+        hook, config = create_hook_and_config(
+            enable_external_plugins=False,
+            enable_logging_configuration=True,
+            print_settings=True,
+        )
+
+        expected_trace_modules = (
+            trace_modules if trace_modules is not None else DEFAULT_TRACE_MODULES
+        )
+        expected_trace_threads = (
+            trace_threads if trace_threads is not None else DEFAULT_TRACE_THREADS
+        )
+
+        assert config.ctrl.trace_modules is expected_trace_modules
+        assert config.ctrl.trace_threads is expected_trace_threads
+
+        plugin = hook.get_plugin('ctrl')
+        assert isinstance(plugin, Plugin)
+        assert plugin._trace_modules is expected_trace_modules
+        assert plugin._trace_threads is expected_trace_threads
