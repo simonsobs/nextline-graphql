2bb9961fd94cc961691ecd4088eb1d97c147d6dd
Tai Sakuma <tai.sakuma@gmail.com>
2025-09-18 12:58:01 -0400

    Add `test_config.py`

---
+++ tests/plugins/graphql/test_config.py
@@ -0,0 +1,32 @@
+from hypothesis import HealthCheck, given, settings
+from pytest import MonkeyPatch
+
+from nextline_test_utils.strategies import st_none_or
+from nextlinegraphql.factory import create_app_for_test
+from nextlinegraphql.plugins.graphql import Plugin
+from tests.app.cors.strategies import st_allow_origins
+
+
+def test_default() -> None:
+    app = create_app_for_test()
+    hook = app.hook
+    config = app.config
+    assert config.graphql.mutation_allow_origins == ['*']
+
+    plugin = hook.get_plugin('graphql')
+    assert isinstance(plugin, Plugin)
+    assert plugin._settings is config
+
+
+@settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
+@given(allow_origins=st_none_or(st_allow_origins()))
+def test_property(allow_origins: list[str] | None, monkeypatch: MonkeyPatch) -> None:
+    with monkeypatch.context() as m:
+        if allow_origins is not None:
+            m.setenv('NEXTLINE_GRAPHQL__MUTATION_ALLOW_ORIGINS', repr(allow_origins))
+
+        app = create_app_for_test()
+        config = app.config
+
+        expected = allow_origins if allow_origins is not None else ['*']
+        assert config.graphql.mutation_allow_origins == expected
